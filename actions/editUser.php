b1c28b6b6ef63bad03d8cc5565b063a56af97514
MFantiniT <matheusfantini@gmail.com>
2024-04-10 10:42:48 -0300

    .

--- actions/editUser.php
+++ actions/editUser.php
@@ -10,7 +10,7 @@ $email = $_POST['email'];
 $id_usuario = $_POST['id_usuario'];
 $nomeArquivoHash = '';
 
-// Verifica se há um arquivo e se o erro de upload é igual a UPLOAD_ERR_OK
+// Verifica se há um arquivo e se o arquivo foi carregado corretamente.
 if (isset($_FILES['img']) && $_FILES['img']['error'] == UPLOAD_ERR_OK) {
     $pastaDestino = "C:\\xampp\\htdocs\\ProjetoFeedz\\img\\";
     $nomeArquivoOriginal = $_FILES['img']['name'];
@@ -23,11 +23,11 @@ if (isset($_FILES['img']) && $_FILES['img']['error'] == UPLOAD_ERR_OK) {
         die('Erro ao mover o arquivo.');
     }
 } else {
-    // Se não houver arquivo enviado, mantém o nome da imagem de perfil atual (se existir)
+    // Se não houver arquivo enviado, mantém o nome da imagem de perfil atual.
     if(isset($_SESSION['img_perfil'])) {
         $nomeArquivoHash = $_SESSION['img_perfil'];
     }
 }
 
-// Chama a função de edição do usuário com o nome da imagem, que pode ser o hash novo ou o antigo
+// Chama a função de edição do usuário com o nome da imagem, que pode ser o hash novo ou o antigo.
 editUser($conn, $id_usuario, $nome, $sobrenome, $email, $nomeArquivoHash);
